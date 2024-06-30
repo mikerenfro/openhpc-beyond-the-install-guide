8cf087bbd9d077afc383fa29e32a8fa5daa07c75
Mike Renfro <renfro@tntech.edu>
2024-06-30 14:13:19 -0500

    grab Tim's delete script

---
+++ delete.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "=== delete.sh"
+
+echo "--- removing known-hosts entries"
+
+OHPC_IP4=$(tofu output -raw ohpc_ipv4)
+if [[ -n "${OHPC_IP4}" ]] ; then
+  ssh-keygen -R $OHPC_IP4
+fi
+
+tofu destroy -auto-approve
