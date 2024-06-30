da9b1c35d32bcd8a65036cd5595415c914e43c2f
Mike Renfro <renfro@tntech.edu>
2024-06-30 15:12:42 -0500

    adjust Tim's delete script to pull correct variable

--- delete.sh
+++ delete.sh
@@ -4,7 +4,7 @@ echo "=== delete.sh"
 
 echo "--- removing known-hosts entries"
 
-OHPC_IP4=$(tofu output -raw ohpc_ipv4)
+OHPC_IP4=$(tofu output -raw ohpc-btig-sms-ipv4)
 if [[ -n "${OHPC_IP4}" ]] ; then
   ssh-keygen -R $OHPC_IP4
 fi
