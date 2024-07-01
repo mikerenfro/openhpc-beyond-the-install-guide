cd55ea1cbeb270da1ae7292342cf59ad2a09f0d5
Mike Renfro <renfro@tntech.edu>
2024-07-01 18:05:46 -0500

    split shared infrastructure and per-user infrastructure

--- ohpc-btig.tf
+++ ohpc-btig.tf
@@ -1,95 +1,11 @@
-# Following along with https://docs.jetstream-cloud.org/ui/cli/overview/
-
-## https://docs.jetstream-cloud.org/ui/cli/managing-ssh-keys/ and
-## https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_keypair_v2
-## Define an SSH public key that will be added to the OpenHPC management node's user's authorized_keys file
-resource "openstack_compute_keypair_v2" "ohpc-btig-keypair" {
-  name       = "ohpc-btig-keypair"
-  public_key = var.ssh_public_key
-}
-
-## https://docs.jetstream-cloud.org/ui/cli/security_group/ and
-## https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_secgroup_v2
-## Define a security group to allow inbound ping and SSH
-resource "openstack_networking_secgroup_v2" "ohpc-btig-ssh-icmp" {
-  name        = "ohpc-btig-ssh-icmp"
-  description = "ssh and icmp enabled"
-}
-## Create SSH access rules from home and from work
-resource "openstack_networking_secgroup_rule_v2" "ohpc-btig-ssh-home" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = var.home_ip_range
-  security_group_id = openstack_networking_secgroup_v2.ohpc-btig-ssh-icmp.id
-}
-resource "openstack_networking_secgroup_rule_v2" "ohpc-btig-ssh-work" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = var.work_ip_range
-  security_group_id = openstack_networking_secgroup_v2.ohpc-btig-ssh-icmp.id
-}
-## Create ICMP access rules from home and from work
-resource "openstack_networking_secgroup_rule_v2" "ohpc-btig-icmp-home" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "icmp"
-  remote_ip_prefix  = var.home_ip_range
-  security_group_id = openstack_networking_secgroup_v2.ohpc-btig-ssh-icmp.id
-}
-resource "openstack_networking_secgroup_rule_v2" "ohpc-btig-icmp-work" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "icmp"
-  remote_ip_prefix  = var.work_ip_range
-  security_group_id = openstack_networking_secgroup_v2.ohpc-btig-ssh-icmp.id
-}
-## Define a security group to allow all traffic on internal network segment
-resource "openstack_networking_secgroup_v2" "ohpc-btig-allow-all" {
-  name        = "ohpc-btig-allow-all"
-  description = "all traffic enabled"
-}
-## Create all-traffic access rules for internal network segment
-resource "openstack_networking_secgroup_rule_v2" "ohpc-btig-allow-all-internal" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  remote_ip_prefix  = "172.16.0.0/16"
-  security_group_id = openstack_networking_secgroup_v2.ohpc-btig-allow-all.id
-}
-
-## https://docs.jetstream-cloud.org/ui/cli/network/ and
-## https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_network_v2
-resource "openstack_networking_network_v2" "ohpc-btig-external-network" {
-  name           = "ohpc-btig-external-network"
-  admin_state_up = "true"
-}
 resource "openstack_networking_network_v2" "ohpc-btig-internal-network" {
   name           = "ohpc-btig-internal-network"
   admin_state_up = "true"
-}
-resource "openstack_networking_subnet_v2" "ohpc-btig-external-subnet" {
-  network_id = openstack_networking_network_v2.ohpc-btig-external-network.id
-  name       = "ohpc-btig-external-subnet"
-  cidr       = "10.38.50.0/24"
 }
 resource "openstack_networking_subnet_v2" "ohpc-btig-internal-subnet" {
   network_id = openstack_networking_network_v2.ohpc-btig-internal-network.id
   name       = "ohpc-btig-internal-subnet"
   cidr       = "172.16.0.0/16"
-}
-resource "openstack_networking_router_v2" "ohpc-btig-router" {
-  name                = "ohpc-btig-router"
-  admin_state_up      = true
-  external_network_id = var.openstack_public_router_id
-}
-resource "openstack_networking_router_interface_v2" "ohpc-btig-router-interface-external-subnet" {
-  router_id = openstack_networking_router_v2.ohpc-btig-router.id
-  subnet_id = openstack_networking_subnet_v2.ohpc-btig-external-subnet.id
 }
 
 ## https://docs.jetstream-cloud.org/ui/cli/launch/ and
@@ -142,22 +58,14 @@ resource "openstack_compute_instance_v2" "ohpc-btig-sms" {
     EOF
 }
 
-## Output
-
-### Create an Ansible inventory on the local system, including the OpenHPC managment node's external IP
-resource "local_file" "ansible" {
-  filename = "ansible/local.ini"
-  content = <<-EOF
-    ## auto-generated
-    [ohpc]
-    sms ansible_host=${openstack_networking_floatingip_v2.ohpc-btig-floating-ip-sms.address} ansible_user=rocky arch=x86_64 ansible_python_interpreter=/usr/bin/python3.9
-
-    [ohpc:vars]
-    # sshkey=${var.ssh_public_key}
-    EOF
-}
-
-### Show the OpenHPC management node's external IPv4 address, so that it can be accessed with "ssh rocky@OHPC_IP"
-output "ohpc-btig-sms-ipv4" {
-  value = openstack_networking_floatingip_v2.ohpc-btig-floating-ip-sms.address
+resource "openstack_compute_instance_v2" "node" {
+  count = 2
+  name = "c${count.index}"
+  image_name = "efi-ipxe"
+  flavor_name = "m3.small"
+  network {
+    uuid = openstack_networking_network_v2.ohpc-btig-internal-network.id
+    fixed_ip_v4 = cidrhost(openstack_networking_subnet_v2.ohpc-btig-internal-subnet.cidr, 256 + count.index)
+  }
+  security_groups = [openstack_networking_secgroup_v2.ohpc-btig-allow-all.name]
 }
