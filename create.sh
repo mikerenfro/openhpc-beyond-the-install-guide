727d5dd20e29c1be1ab16a07b229668505e1ed43
Mike Renfro <renfro@tntech.edu>
2024-07-01 21:43:06 -0500

    correct typo to use individual IP for ssh-keygen -R

--- create.sh
+++ create.sh
@@ -10,7 +10,7 @@ echo "--- removing known-hosts entries"
 OHPC_IP4=$(tofu output -json ohpc-btig-sms-ipv4 | jq -r '.[]')
 if [[ -n "${OHPC_IP4}" ]] ; then
   for IP in ${OHPC_IP4}; do
-    ssh-keygen -R $OHPC_IP4
+    ssh-keygen -R $IP
   done
 fi
 
