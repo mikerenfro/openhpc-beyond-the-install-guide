81c93176459fa5c937b3c2911e7f762d1c155f57
Mike Renfro <renfro@tntech.edu>
2024-07-02 17:48:34 -0500

    correct loop logic for grabbing MACs from OpenStack and putting them into Ansible host vars

--- macs_to_host_vars.sh
+++ macs_to_host_vars.sh
@@ -3,11 +3,12 @@
 cluster_numbers=$(tofu output -json ohpc-btig-macs | jq -r 'keys[] as $k | "\($k)"' | cut -d- -f1 | sort | uniq | sed 's/cluster//g')
 for n in $cluster_numbers; do
     host_var_file=ansible/host_vars/sms-${n}
-    echo "macs:" > ${host_var_file}
+    echo ${host_var_file}
+    echo "compute_nodes:" > ${host_var_file}
+    i=1
     for n in $(tofu output -json ohpc-btig-macs | jq "keys[] as \$k | \$k | select(match(\"cluster${n}\"))"); do
-        macs=$(tofu output -json ohpc-btig-macs | jq -r ".[$n][0]")
-        for mac in $macs; do
-            echo "- ${mac}" >> ${host_var_file}
-        done
+        mac=$(tofu output -json ohpc-btig-macs | jq -r ".[$n][0]")
+        echo "- { name: \"c${i}\", mac: \"${mac}\" }" >> ${host_var_file}
+        ((i++))
     done
 done
