abe1ed0b21186378c403df6d15198fc548104bd9
Mike Renfro <renfro@tntech.edu>
2024-07-04 18:37:52 -0500

    correctly add password-secured admin users to each sms

--- passwords_to_host_vars.sh
+++ passwords_to_host_vars.sh
@@ -4,6 +4,7 @@ if [ ! -f user-passwords.txt ]; then
     echo "ERROR: see $0 for instructions on generating the user passwords."
     exit 1
 fi
+users_per_host=2
 # Generate students' plain-text passwords from https://beta.xkpasswd.net ,
 # store them in user-passwords.txt , then convert them into salted hashes
 # for ansible host_vars files, one file per student. Make sure there's an
@@ -14,8 +15,10 @@ while read p ; do
     host_var_file=ansible/host_vars/sms-${i}
     if [ -f ${host_var_file} ]; then
         pc=$(python -c "import crypt,getpass; print(crypt.crypt('$p', crypt.mksalt(crypt.METHOD_SHA512)))")
-        echo "user1_password: $pc" >> ${host_var_file}
-        echo "user2_password: $pc" >> ${host_var_file}
+        echo "user_creds:" >> ${host_var_file}
+        for n in $(seq 1 ${users_per_host}); do
+            echo "- { username: 'user${n}', password: '$pc' }" >> ${host_var_file}
+        done
         ((i++))
     else
         break
